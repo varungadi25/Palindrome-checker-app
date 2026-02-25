d09a9fe087c23dca0e85f9e4b2b2b09f318fd625
varungadi25 <varungadi@gmail.com>
2026-02-25 15:53:42 +0530

    uc2

--- Palindromechecker/src/Palindromechecker.java
+++ Palindromechecker/src/Palindromechecker.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
-
 public class Palindromechecker {
+    public static void main(String[] args) {
+
+        String input = "madam";
+        boolean isPalindrome = true;
+
+        // Loop only till half of the string length
+        for (int i = 0; i < input.length() / 2; i++) {
+
+            if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        System.out.println("Input text: " + input);
+        System.out.println("Is it a Palindrome? : " + isPalindrome);
+    }
 }
+
