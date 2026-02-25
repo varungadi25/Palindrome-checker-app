6993a96fc2c5741040bee485ec7bb556ee3d9332
varungadi25 <varungadi@gmail.com>
2026-02-25 15:44:09 +0530

    UC1

---
+++ Palindromechecker/src/Palindromechecker.java
@@ -0,0 +1,18 @@
+public class Palindromechecker {
+        public static void main(String args[]) {
+            System.out.println("============================================");
+            System.out.println(" welcome to Palindrome checker app");
+            System.out.println("============================================");
+            System.out.println("Application Name : Palindrome Checker app");
+            System.out.println("Version: 1.0");
+            System.out.println("Developer:  RA928");
+            System.out.println("==============================================");
+            System.out.println("Application started successfully");
+            System.out.println("==============================================");
+
+
+
+
+    }
+
+}
